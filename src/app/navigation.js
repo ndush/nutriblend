156d441c0864798c7100ab261a33096700bf356d
ndush <mdamarisnduku@gmail.com>
2024-09-09 03:32:55 +0300

    add navigation

---
+++ src/app/navigation.js
@@ -0,0 +1,14 @@
+// src/app/navigation.js
+
+export default function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li><a href="/meal-suggestions">Meal Suggestions</a></li>
+        <li><a href="/ingredient-search">Search Ingredients</a></li>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </nav>
+  );
+}
